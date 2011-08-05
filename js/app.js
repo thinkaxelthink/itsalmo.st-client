2c20e06e2f9aff583308a4c1ebd7808adeb71ace
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2011-08-05 13:39:58 -0400

    added some more core structure

---
+++ js/app.js
@@ -0,0 +1,57 @@
+/*    _____                    _____          
+     /\    \                  /\    \         
+    /::\    \                /::\    \        
+    \:::\    \              /::::\    \       
+     \:::\    \            /::::::\    \      
+      \:::\    \          /:::/\:::\    \     
+       \:::\    \        /:::/  \:::\    \    
+       /::::\    \      /:::/    \:::\    \   
+      /::::::\    \    /:::/    / \:::\    \  
+     /:::/\:::\    \  /:::/    /   \:::\    \ 
+    /:::/  \:::\____\/:::/____/     \:::\____\
+   /:::/    \::/    /\:::\    \      \::/    /
+  /:::/    / \/____/  \:::\    \      \/____/ 
+ /:::/    /            \:::\    \             
+/:::/    /              \:::\    \            
+\::/    /                \:::\    \           
+ \/____/                  \:::\    \          
+                           \:::\    \         
+                            \:::\____\        
+                             \::/    /        
+                              \/____/         
+                             TYPE/CODE        
+                         From 2010 till ∞     */
+
+if(!tc){ var tc = {}; }
+
+tc.jQ = jQuery;
+
+jQuery(document).bind('ready',function(){
+	var app = new tc.app(page);
+});
+
+tc.app = function(page){
+	var me;
+	me = this;
+	this.events = $({});
+	this.init(page);
+};
+
+tc.app.prototype.init = function(page){
+	var me;
+	me = this;
+	if(page.features){
+		for(i in page.features){
+			if(tc.jQ.isFunction(page.features[i])){
+				if(page.features[i](me) === false){
+					break;
+				}
+			}
+		}
+		
+		this.events.trigger('app.featuresInitialized');
+	}
+};
+
+tc.app.prototype.components = {};
+tc.app.prototype.events = tc.jQ({});
