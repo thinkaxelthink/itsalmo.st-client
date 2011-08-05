f924dc103c3f5da30a377a120221c539cc2210e4
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2011-08-05 16:02:06 -0400

    more boilerplate

---
+++ js/app/feature.hashchange.js
@@ -0,0 +1,14 @@
+(function(window) {
+	
+	var page = window.page,
+			log = new NI.Logging({
+				moduleName:'Feature(Hashchange)',
+				enabled:true
+			});
+
+	page.features.push(function(app){
+		
+		
+	});
+	
+})(window);
