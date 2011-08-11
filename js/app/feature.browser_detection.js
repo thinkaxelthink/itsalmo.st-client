3af80054c0f22cd3f3c5f76df30a4f64443b5c7b
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2011-08-11 13:01:13 -0400

    some cool stuff

--- js/app/feature.browser_detection.js
+++ js/app/feature.browser_detection.js
@@ -7,8 +7,24 @@
 			});
 			
 	page.features.push(function(app){
+		var browser_header;
 		
 		app.browserDetection = new NI.BrowserDetection({});
+		browser_header = $('.browser-invalid');
+		
+		browser_header.find('.browser-close').bind('click',function(e,d){
+			e.preventDefault();
+			browser_header.hide();
+		});
+		
+		if($.browser.msie || $.browser.opera){
+			browser_header.show().animate({
+				top:'0px'
+			},600,function(){
+				
+			});
+		}
+		
 		
 	});
 	
